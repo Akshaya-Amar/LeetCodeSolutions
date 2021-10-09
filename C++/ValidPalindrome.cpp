b55972c042d032572900e134cf831a498a45b8de
akshayaamar <akshayaamar05@gmail.com>
2021-10-09 16:42:46 +0530

    second

---
+++ C++/ValidPalindrome.cpp
@@ -0,0 +1,43 @@
+/*
+
+Source: https://leetcode.com/problems/valid-palindrome/submissions/
+
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+*/
+
+class Solution {
+public:
+  bool isPalindrome(string s) {
+
+    int head = 0;
+    int tail = s.size() - 1;
+
+    while(head < tail){
+
+      char ch1 = s[head];
+      while(head < tail && !(isAlphaNumeric(ch1))) {
+        ch1 = s[++head];
+      }
+
+      char ch2 = s[tail];
+      while(tail > head && !(isAlphaNumeric(ch2))) {
+        ch2 = s[--tail];
+      }
+
+      if(ch1 != ch2 && ch1 != (ch2 ^ 32)) {
+        return false;
+      }
+
+      ++head;
+      --tail;
+    }
+
+    return true;
+  }
+
+private:
+  bool isAlphaNumeric(char ch){
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
+  }
+};
---
+++ Java/ValidPalindrome.java
@@ -0,0 +1,41 @@
+/*
+
+Source: https://leetcode.com/problems/valid-palindrome/submissions/
+
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+*/
+
+class Solution {
+  public boolean isPalindrome(String s) {
+
+    int head = 0;
+    int tail = s.length() - 1;
+
+    while(head < tail){
+
+      char ch1 = s.charAt(head);
+      while(head < tail && !(isAlphaNumeric(ch1))) {
+        ch1 = s.charAt(++head);
+      }
+
+      char ch2 = s.charAt(tail);
+      while(tail > head && !(isAlphaNumeric(ch2))) {
+        ch2 = s.charAt(--tail);
+      }
+
+      if(ch1 != ch2 && ch1 != (ch2 ^ 32)) {
+        return false;
+      }
+
+      ++head;
+      --tail;
+    }
+
+    return true;
+  }
+
+  public boolean isAlphaNumeric(char ch){
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
+  }
+}
