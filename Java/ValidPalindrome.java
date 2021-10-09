a67632a7c1497995e9e1d336fbf8926b0bf25cb9
akshayaamar <akshayaamar05@gmail.com>
2021-10-09 16:49:28 +0530

    details added

--- Java/ValidPalindrome.java
+++ Java/ValidPalindrome.java
@@ -3,17 +3,18 @@
 Source: https://leetcode.com/problems/valid-palindrome/submissions/
 
 Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
 Example 1:
 
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
 Explanation: "amanaplanacanalpanama" is a palindrome.
+
 Example 2:
 
 Input: s = "race a car"
 Output: false
 Explanation: "raceacar" is not a palindrome.
-
 
 Constraints:
 
