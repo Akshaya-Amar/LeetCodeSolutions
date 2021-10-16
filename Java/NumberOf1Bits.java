8a89f89a6c2ad49b2aae7be79c1ab364d9b26c3d
Akshaya Amar <88337278+Akshaya-Amar@users.noreply.github.com>
2021-10-16 12:41:42 +0530

    Update NumberOf1Bits.java

--- Java/NumberOf1Bits.java
+++ Java/NumberOf1Bits.java
@@ -2,6 +2,10 @@
 
 Source: https://leetcode.com/problems/number-of-1-bits
 
+COMPLEXITY:
+Time: O(1), maximum number of possible signed bits in int type are 32, so O(32) ~ O(1)
+Space: O(1), in-place
+
 */
 public class Solution {
     public int hammingWeight(int n) {
