4f671abfcf8c1b295d16a573e04f6b670ae46f91
akshayaamar <akshayaamar05@gmail.com>
2021-12-08 22:52:31 +0530

    wedupdate

--- C++/NumberOf1Bits.cpp
+++ C++/NumberOf1Bits.cpp
@@ -2,18 +2,23 @@
 
 Source: https://leetcode.com/problems/number-of-1-bits
 
+COMPLEXITY:
+Time: O(1), maximum number of possible signed bits in int type are 32, so O(32) ~ O(1)
+Space: O(1), in-place
+
 */
+
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
+  int hammingWeight(uint32_t n) {
 
-        int setBitCount = 0;
+    int setBitCount = 0;
 
-        while(n != 0) {
-            n &= n - 1;
-            ++setBitCount; // to clear the rightmost set bit
-        }
-
-        return setBitCount;
+    while(n != 0) {
+      n &= n - 1;
+      ++setBitCount; // to clear the rightmost set bit
     }
+
+    return setBitCount;
+  }
 };
--- Java/NumberOf1Bits.java
+++ Java/NumberOf1Bits.java
@@ -7,16 +7,17 @@ Time: O(1), maximum number of possible signed bits in int type are 32, so O(32) 
 Space: O(1), in-place
 
 */
+
 public class Solution {
-    public int hammingWeight(int n) {
+  public int hammingWeight(int n) {
 
-        int setBitCount = 0;
+    int setBitCount = 0;
 
-        while(n != 0){
-            n &= (n - 1); // to clear the rightmost set bit
-            ++setBitCount;
-        }
-
-        return setBitCount;
+    while(n != 0){
+      n &= (n - 1); // to clear the rightmost set bit
+      ++setBitCount;
     }
+
+    return setBitCount;
+  }
 }
