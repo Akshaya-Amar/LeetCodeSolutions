66195f0459b34c71d5e91b1d18ac99b89ffe7ae1
akshayaamar <akshayaamar05@gmail.com>
2021-12-18 23:41:13 +0530

    satupdatedec18

--- C++/SumofDigitsofStringAfterConvert.cpp
+++ C++/SumofDigitsofStringAfterConvert.cpp
@@ -6,3 +6,35 @@ Time: O(n + k), where n is the length of the given string(s) and k is the given 
 Space: O(1), in-place
 
 */
+
+class Solution {
+public:
+  int getLucky(string s, int k) {
+
+    int res = 0;
+    for(char ch : s) {
+
+      int val = ch - 96;
+
+      if(val > 9) {
+        res += (val / 10) + (val % 10);
+      } else {
+        res += val;
+      }
+    }
+
+    while(--k > 0 && res > 9) {
+
+      int sumOfDigits = 0;
+
+      while(res != 0) {
+        sumOfDigits += res % 10;
+        res /= 10;
+      }
+
+      res = sumOfDigits;
+    }
+
+    return res;
+  }
+};
