592325cd2b40fa84dfd92ad357bf2b59a9c0d811
akshayaamar <akshayaamar05@gmail.com>
2021-12-22 21:19:46 +0530

    wedupdatedec22

---
+++ Java/KthDistinctStringinanArray.java
@@ -0,0 +1,8 @@
+/*
+
+Source: https://leetcode.com/problems/kth-distinct-string-in-an-array/
+
+Time: O(n), where n is the size of the given string array(arr)
+Space: O(n), we need a hashmap for storing the boolean values of each string in the given string array(arr)
+
+*/
