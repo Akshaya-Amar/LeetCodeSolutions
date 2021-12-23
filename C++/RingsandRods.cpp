f424683ef3e3455c9a5ae4d0b244830ec286db61
akshayaamar <akshayaamar05@gmail.com>
2021-12-23 23:57:58 +0530

    thurudpate23dec4

--- C++/RingsandRods.cpp
+++ C++/RingsandRods.cpp
@@ -11,17 +11,17 @@ class Solution {
 public:
   int countPoints(string rings) {
 
-    int colorCodes[10] = {0};
+    int rods[10] = {0};
     int len = rings.length();
 
     for(int i = 0; i < len; i += 2) {
 
-      colorCodes[rings[i + 1] - '0'] |= getColorCode(rings[i]);
+      rods[rings[i + 1] - '0'] |= getColorCode(rings[i]);
     }
 
     int rodsWithAllColors = 0;
-    for(int colorCode : colorCodes) {
-      if(colorCode == 7) {
+    for(int colorCodesSum : rods) {
+      if(colorCodesSum == 7) {
         ++rodsWithAllColors;
       }
     }
--- Java/RingsandRods.java
+++ Java/RingsandRods.java
@@ -10,17 +10,17 @@ Space: O(1), i.e. in-place.
 class Solution {
   public int countPoints(String rings) {
 
-    int[] colorCodes = new int[10];
+    int[] rods = new int[10];
     int len = rings.length();
 
     for(int i = 0; i < len; i += 2) {
 
-      colorCodes[rings.charAt(i + 1) - '0'] |= getColorCode(rings.charAt(i));
+      rods[rings.charAt(i + 1) - '0'] |= getColorCode(rings.charAt(i));
     }
 
     int rodsWithAllColors = 0;
-    for(int colorCode : colorCodes) {
-      if(colorCode == 7) {
+    for(int colorCodesSum : rods) {
+      if(colorCodesSum == 7) {
         ++rodsWithAllColors;
       }
     }
