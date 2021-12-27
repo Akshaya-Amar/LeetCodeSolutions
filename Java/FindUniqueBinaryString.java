8d933057ade05291c669a6e5b5ce44bafd5db186
Akshaya Amar <88337278+Akshaya-Amar@users.noreply.github.com>
2021-12-27 22:20:38 +0530

    Update FindUniqueBinaryString.java

--- Java/FindUniqueBinaryString.java
+++ Java/FindUniqueBinaryString.java
@@ -3,7 +3,7 @@
 Source: https://leetcode.com/problems/find-unique-binary-string/
 
 Time: O(n), where n is the size of given string array(nums)
-Space: O(1), in-place, though we are using extra char array to assign desired character('0' or '1') at their appropriate place
+Space: O(1), in-place, though we are using extra char array(ch) to assign desired character('0' or '1') at their appropriate place
 but we will still treat space as O(1) and not O(n) because we usually don't consider the output in the space
 complexity i.e. only temporary spaces which are used to get the desired output are considered.
 
