ff616065bfe319a69e5d4c5b80e5b307ba889075
akshayaamar <akshayaamar05@gmail.com>
2022-01-07 22:59:17 +0530

    friupdate7jan

---
+++ C++/MaximumNumberofWordsFoundinSentences.cpp
@@ -0,0 +1,32 @@
+/*
+
+Source: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/submissions/
+
+Time: O(n ^ m), where n is the size of the given string array(sentences) and m is the average length of all sentences in array
+Space: O(1), in-place
+
+*/
+
+class Solution {
+public:
+  int mostWordsFound(vector<string>& sentences) {
+
+    int max = 0;
+    for(string sentence : sentences) {
+
+      int spaceCount = 1;
+      int len = sentence.length();
+      for(int i = 0; i < len; ++i) {
+        if(sentence[i] == ' ') {
+          ++spaceCount;
+        }
+      }
+
+      if(spaceCount > max) {
+        max = spaceCount;
+      }
+    }
+
+    return max;
+  }
+};
---
+++ Java/MaximumNumberofWordsFoundinSentences.java
@@ -0,0 +1,30 @@
+/*
+
+Source: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/submissions/
+
+Time: O(n ^ m), where n is the size of the given string array(sentences) and m is the average length of all sentences in array
+Space: O(1), in-place
+
+*/
+
+class Solution {
+  public int mostWordsFound(String[] sentences) {
+
+    int max = 0;
+    for(String sentence : sentences) {
+
+      int spaceCount = 1;
+      for(char ch : sentence.toCharArray()) {
+        if(ch == ' ') {
+          ++spaceCount;
+        }
+      }
+
+      if(spaceCount > max) {
+        max = spaceCount;
+      }
+    }
+
+    return max;
+  }
+}
