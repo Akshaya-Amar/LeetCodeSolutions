d48f16ceb8307a559b149d9b0c4bd16ec0341d98
akshayaamar <akshayaamar05@gmail.com>
2022-01-13 19:52:34 +0530

    thuupate13jan1

---
+++ C++/ComplementofBase10Integer.cpp
@@ -0,0 +1,31 @@
+/*
+
+Source: https://leetcode.com/problems/complement-of-base-10-integer/
+
+Time: O(log base2 n), where n is the given number(num) and (log base2 n) means that the loop will iterate for a maximum of (log base2 n) times.
+i.e. if n = 10, then the loop will iterate for 4((logn + 1) ~ logn) times. 6 times in case if n = 32.
+Space: O(1), in-place
+
+*/
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+
+        if(n == 0) {
+            return 1;
+        }
+
+        int complement = n;
+        int mask = 1;
+
+        while(n != 0) {
+
+            complement ^= mask;
+            mask <<= 1;
+            n >>= 1;
+        }
+
+        return complement;
+    }
+};
