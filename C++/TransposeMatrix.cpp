a597c3aa36df4d4fda49d068f405de24750b46f1
akshayaamar <akshayaamar05@gmail.com>
2022-01-19 00:12:32 +0530

    wedupdate19jan3

--- C++/TransposeMatrix.cpp
+++ C++/TransposeMatrix.cpp
@@ -16,9 +16,9 @@ public:
     int n = matrix[0].size();
 
     vector<vector<int>> res(n, vector<int>(m));
-    for(int i = 0; i < m; ++i) {
-      for(int j = 0; j < n; ++j) {
-        res[j][i] = matrix[i][j];
+    for(int i = 0; i < n; ++i) {
+      for(int j = 0; j < m; ++j) {
+        res[i][j] = matrix[j][i];
       }
     }
 
--- Java/TransposeMatrix.java
+++ Java/TransposeMatrix.java
@@ -15,9 +15,9 @@ class Solution {
     int n = matrix[0].length;
 
     int[][] res = new int[n][m];
-    for(int i = 0; i < m; ++i) {
-      for(int j = 0; j < n; ++j) {
-        res[j][i] = matrix[i][j];
+    for(int i = 0; i < n; ++i) {
+      for(int j = 0; j < m; ++j) {
+        res[i][j] = matrix[j][i];
       }
     }
 
