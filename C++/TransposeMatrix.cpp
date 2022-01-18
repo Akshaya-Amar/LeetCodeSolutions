838a76768bebdedbd0be4d5dd8e774c7598a713f
akshayaamar <akshayaamar05@gmail.com>
2022-01-18 23:50:12 +0530

    tueupdate18jan1

---
+++ C++/TransposeMatrix.cpp
@@ -0,0 +1,27 @@
+/*
+
+Source: https://leetcode.com/problems/transpose-matrix/
+
+Time: O(m * n), where m and n are the lengths of rows and columns respectively
+Space: O(1), in-place, though we are using extra 2d array(res) but we will still treat space as O(1) and not O(n)
+because we usually don't consider the output in the space complexity i.e. only temporary spaces which are used to get the desired output are considered
+
+*/
+
+class Solution {
+public:
+  vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<vector<int>> res(n, vector<int>(m));
+    for(int i = 0; i < m; ++i) {
+      for(int j = 0; j < n; ++j) {
+        res[j][i] = matrix[i][j];
+      }
+    }
+
+    return res;
+  }
+};
---
+++ Java/TransposeMatrix.java
@@ -0,0 +1,26 @@
+/*
+
+Source: https://leetcode.com/problems/transpose-matrix/
+
+Time: O(m * n), where m and n are the lengths of rows and columns respectively
+Space: O(1), in-place, though we are using extra 2d array(res) but we will still treat space as O(1) and not O(n)
+because we usually don't consider the output in the space complexity i.e. only temporary spaces which are used to get the desired output are considered
+
+*/
+
+class Solution {
+  public int[][] transpose(int[][] matrix) {
+
+    int m = matrix.length;
+    int n = matrix[0].length;
+
+    int[][] res = new int[n][m];
+    for(int i = 0; i < m; ++i) {
+      for(int j = 0; j < n; ++j) {
+        res[j][i] = matrix[i][j];
+      }
+    }
+
+    return res;
+  }
+}
