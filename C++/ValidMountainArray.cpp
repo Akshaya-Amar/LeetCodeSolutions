aa22ae97cdd5fbf9bb5e11f28742b732d4bced7a
akshayaamar <akshayaamar05@gmail.com>
2022-01-26 23:27:53 +0530

    wedupdate26jan2

---
+++ C++/ValidMountainArray.cpp
@@ -0,0 +1,32 @@
+/*
+
+Source: https://leetcode.com/problems/valid-mountain-array/
+
+Time: O(log base2 n), where n is the size of the array
+Space: O(1), in-place
+
+*/
+
+class Solution {
+public:
+  bool validMountainArray(vector<int>& arr) {
+
+    int size = arr.size();
+    if(size < 3) {
+      return false;
+    }
+
+    int i = 0;
+    while(++i < size && arr[i] > arr[i - 1]);
+
+    if(i == 1 || i == size) {
+      return false;
+    }
+
+    while(i < size && arr[i] < arr[i - 1]) {
+      ++i;
+    }
+
+    return i == size;
+  }
+};
---
+++ Java/ValidMountainArray.java
@@ -0,0 +1,31 @@
+/*
+
+Source: https://leetcode.com/problems/valid-mountain-array/
+
+Time: O(n), where n is the length of the array
+Space: O(1), in-place
+
+*/
+
+class Solution {
+  public boolean validMountainArray(int[] arr) {
+
+    int len = arr.length;
+    if(len < 3) {
+      return false;
+    }
+
+    int i = 0;
+    while(++i < len && arr[i] > arr[i - 1]);
+
+    if(i == 1 || i == len) {
+      return false;
+    }
+
+    while(i < len && arr[i] < arr[i - 1]) {
+      ++i;
+    }
+
+    return i == len;
+  }
+}
