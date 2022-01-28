3d15939a8f710b48c89377d4917f55ffdd4b42a0
akshayaamar <akshayaamar05@gmail.com>
2022-01-29 00:17:00 +0530

    added solution without comments too for SearchinRotatedSortedArray problem

--- C++/SearchinRotatedSortedArray.cpp
+++ C++/SearchinRotatedSortedArray.cpp
@@ -7,6 +7,7 @@ Space: O(1), in-place
 
 */
 
+// commented solution
 class Solution {
 public:
   int search(vector<int>& nums, int target) {
@@ -24,19 +25,89 @@ public:
 
       int mid = (start + end) >> 1;
 
-      if(mid > 0 && nums[mid] < nums[mid - 1]) { // if the current element is minimum, then store it's index and stop iteration
+      // if the current element is minimum, then store it's index and stop iteration
+      if(mid > 0 && nums[mid] < nums[mid - 1]) {
+      start = mid;
+      break;
+    }
+
+    if(nums[mid] > nums[end]) { // to search in right sorted part
+      start = mid + 1;
+    } else { // to search in left sorted part
+      end = mid;
+    }
+  }
+
+  // if the minimum element is equal to the the target, then directly return it's index
+  int indexOfMinElement = start;
+  if(nums[indexOfMinElement] == target) {
+    return indexOfMinElement;
+  }
+
+  start = 0;
+  end = nums.size() - 1;
+
+  // if the target element is on the right sorted part, then
+  if(target > nums[indexOfMinElement] && target <= nums[end]) {
+    start = indexOfMinElement; // search from index of minimum element to the end of the array(nums)
+  } else { // if the target element is on the left side of the sorted array
+    end = indexOfMinElement - 1; // search from 0 to one previous element from index of minimum element
+  }
+
+  // search for the index of target if the target is present in the nums, else return -1
+  return indexOfTarget(nums, start, end, target);
+}
+
+private:
+  int indexOfTarget(vector<int>& nums, int start, int end, int target) {
+
+    while(start <= end) {
+
+      int mid = (start + end) >> 1;
+
+      if(nums[mid] == target) {
+        return mid;
+      }
+
+      if(nums[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+};
+
+
+// solution without comments
+class Solution {
+public:
+  int search(vector<int>& nums, int target) {
+
+    int end = nums.size() - 1;
+    if(nums[0] < nums[end]) {
+      return indexOfTarget(nums, 0, end, target);
+    }
+
+    int start = 0;
+    while(start < end) {
+
+      int mid = (start + end) >> 1;
+
+      if(mid > 0 && nums[mid] < nums[mid - 1]) {
         start = mid;
         break;
       }
 
-      if(nums[mid] > nums[end]) { // to search in right sorted part
+      if(nums[mid] > nums[end]) {
         start = mid + 1;
       } else {
-        end = mid; // to search in left sorted part
+        end = mid;
       }
     }
 
-    // if the minimum element is equal to the the target, then directly return it's index
     int indexOfMinElement = start;
     if(nums[indexOfMinElement] == target) {
       return indexOfMinElement;
@@ -44,13 +115,12 @@ public:
 
     start = 0;
     end = nums.size() - 1;
-    if(target > nums[indexOfMinElement] && target <= nums[end]) { // if the target element is on the right sorted part, then
-      start = indexOfMinElement; // start searching from index of minimum element to the end of the array(nums)
-    } else { // to check if the target element is on the left side of the sorted array
-      end = indexOfMinElement - 1; // start searching from 0 to one previous element from index of minimum element
+    if(target > nums[indexOfMinElement] && target <= nums[end]) {
+      start = indexOfMinElement;
+    } else {
+      end = indexOfMinElement - 1;
     }
 
-    // search for the index of target if the target is present in the nums, else return -1
     return indexOfTarget(nums, start, end, target);
   }
 
--- Java/SearchinRotatedSortedArray.java
+++ Java/SearchinRotatedSortedArray.java
@@ -7,6 +7,7 @@ Space: O(1), in-place
 
 */
 
+// commented solution
 class Solution {
   public int search(int[] nums, int target) {
 
@@ -23,15 +24,16 @@ class Solution {
 
       int mid = (start + end) >> 1;
 
-      if(mid > 0 && nums[mid] < nums[mid - 1]) { // if the current element is minimum, then store it's index and stop iteration
+      // if the current element is minimum, then store it's index and stop iteration
+      if(mid > 0 && nums[mid] < nums[mid - 1]) {
         start = mid;
         break;
       }
 
       if(nums[mid] > nums[end]) { // to search in right sorted part
         start = mid + 1;
-      } else {
-        end = mid; // to search in left sorted part
+      } else { // to search in left sorted part
+        end = mid;
       }
     }
 
@@ -43,10 +45,12 @@ class Solution {
 
     start = 0;
     end = nums.length - 1;
-    if(target > nums[indexOfMinElement] && target <= nums[end]) { // if the target element is on the right sorted part, then
-      start = indexOfMinElement; // start searching from index of minimum element to the end of the array(nums)
-    } else { // to check if the target element is on the left side of the sorted array
-      end = indexOfMinElement - 1; // start searching from 0 to one previous element from index of minimum element
+
+    // if the target element is on the right sorted part, then
+    if(target > nums[indexOfMinElement] && target <= nums[end]) {
+      start = indexOfMinElement; // search from index of minimum element to the end of the array(nums)
+    } else { // if the target element is on the left side of the sorted array
+      end = indexOfMinElement - 1; // search from 0 to one previous element from index of minimum element
     }
 
     // search for the index of target if the target is present in the nums, else return -1
@@ -54,6 +58,69 @@ class Solution {
   }
 
   // standard binary search
+  private int indexOfTarget(int[] nums, int start, int end, int target) {
+
+    while(start <= end) {
+
+      int mid = (start + end) >> 1;
+
+      if(nums[mid] == target) {
+        return mid;
+      }
+
+      if(nums[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+}
+
+// solution without comments
+class Solution {
+  public int search(int[] nums, int target) {
+
+    int end = nums.length - 1;
+    if(nums[0] < nums[end]) {
+      return indexOfTarget(nums, 0, end, target);
+    }
+
+    int start = 0;
+    while(start < end) {
+
+      int mid = (start + end) >> 1;
+
+      if(mid > 0 && nums[mid] < nums[mid - 1]) {
+        start = mid;
+        break;
+      }
+
+      if(nums[mid] > nums[end]) {
+        start = mid + 1;
+      } else {
+        end = mid;
+      }
+    }
+
+    int indexOfMinElement = start;
+    if(nums[indexOfMinElement] == target) {
+      return indexOfMinElement;
+    }
+
+    start = 0;
+    end = nums.length - 1;
+    if(target > nums[indexOfMinElement] && target <= nums[end]) {
+      start = indexOfMinElement;
+    } else {
+      end = indexOfMinElement - 1;
+    }
+
+    return indexOfTarget(nums, start, end, target);
+  }
+
   private int indexOfTarget(int[] nums, int start, int end, int target) {
 
     while(start <= end) {
