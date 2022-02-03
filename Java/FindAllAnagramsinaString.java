7b2a083aba434c568df49408b23e00db4aba83b2
akshayaamar <akshayaamar05@gmail.com>
2022-02-03 23:47:22 +0530

    findallanagramsinstring program added

---
+++ Java/FindAllAnagramsinaString.java
@@ -0,0 +1,49 @@
+/*
+
+Source: https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+Time: O(n), where n is the length of the given string(s)
+Space: O(n)
+
+*/
+
+class Solution {
+  public List<Integer> findAnagrams(String s, String p) {
+
+    int sLen = s.length();
+    int pLen = p.length();
+
+    if(sLen < pLen) {
+      return new ArrayList<>();
+    }
+
+    char[] hash = new char[26];
+
+    for(int i = 0; i < pLen; i++) {
+      hash[p.charAt(i) - 'a']++;
+    }
+
+    int left = 0;
+    int right = 0;
+    List<Integer> pos = new ArrayList<>();
+
+    while(right < sLen) {
+
+      int index = s.charAt(right) - 'a';
+
+      if(hash[index] > 0) {
+        --hash[index];
+        ++right;
+      } else {
+        ++hash[s.charAt(left) - 'a'];
+        ++left;
+      }
+
+      if(right - left == pLen) {
+        pos.add(left);
+      }
+    }
+
+    return pos;
+  }
+}
