c25ef6a8b26b41befdf6fdc5c36136634cf38274
akshayaamar <akshayaamar05@gmail.com>
2022-02-10 14:18:33 +0530

    K-diffPairsinanArray program added

---
+++ Java/K-diffPairsinanArray.java
@@ -0,0 +1,28 @@
+/*
+
+Source: https://leetcode.com/problems/k-diff-pairs-in-an-array/
+
+Time: O(n), where n is the length of the given array(nums)
+Space: O(n), we need a map to store the elements of array(nums)
+
+*/
+
+class Solution {
+  public int findPairs(int[] nums, int k) {
+
+    Map<Integer, Integer> map = new HashMap<>();
+    for(Integer num : nums) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    int count = 0;
+    for(Integer key : map.keySet()) {
+
+      if((k > 0 && map.containsKey(key + k)) || (k == 0 && map.get(key) > 1)) {
+        ++count;
+      }
+    }
+
+    return count;
+  }
+}
