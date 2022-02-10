5c41e670224bce337dee53e3caf531a6ee138d42
akshayaamar <akshayaamar05@gmail.com>
2022-02-10 14:28:24 +0530

    change in K-diffPairsinanArray program

--- Java/K-diffPairsinanArray.java
+++ Java/K-diffPairsinanArray.java
@@ -11,7 +11,7 @@ class Solution {
   public int findPairs(int[] nums, int k) {
 
     Map<Integer, Integer> map = new HashMap<>();
-    for(Integer num : nums) {
+    for(int num : nums) {
       map.put(num, map.getOrDefault(num, 0) + 1);
     }
 
