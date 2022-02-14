807e924f5c3f2ac7b593515514620c94aa37456e
akshayaamar <akshayaamar05@gmail.com>
2022-02-14 11:44:31 +0530

    Most Optimized solution added for permutation in string

--- C++/PermutationinString.cpp
+++ C++/PermutationinString.cpp
@@ -2,7 +2,9 @@
 
 Source: https://leetcode.com/problems/permutation-in-string/
 
-Time: O(n * m), where n and m are the lengths of the given string s2 and total number lower case english letters(26) respectively
+1st approach
+
+Time: O(m * n), where m is total number lower case english letters(26) and n is the length of the given string s2
 Space: O(1), in-place
 
 */
@@ -36,5 +38,114 @@ public:
     }
 
     return s1CharFreq == s2CharFreq;
+  }
+};
+
+
+/*
+
+2nd approach (More optimized as we are using only a single vector(charFreq))
+
+Time: (n * m), where m is total number lower case english letters(26) and n is the length of the given string s2
+Space: O(1), in-place
+
+*/
+
+class Solution {
+public:
+  bool checkInclusion(string s1, string s2) {
+
+    int s1Len = s1.length();
+    int s2Len = s2.length();
+
+    if(s1Len > s2Len) {
+      return false;
+    }
+
+    vector<int> charFreq(26, 0);
+    for(int i = 0; i < s1Len; ++i) {
+      ++charFreq[s1[i] - 'a'];
+      --charFreq[s2[i] - 'a'];
+    }
+
+    for(int i = s1Len; i < s2Len; ++i) {
+
+      if(isPerm(charFreq)) {
+        return true;
+      }
+
+      --charFreq[s2[i] - 'a'];
+      ++charFreq[s2[i - s1Len] - 'a'];
+    }
+
+    return isPerm(charFreq);
+  }
+
+private:
+  bool isPerm(vector<int>& charFreq) {
+
+    for(int i = 0; i < 26; ++i) {
+
+      if(charFreq[i] != 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
+
+
+
+/*
+
+3rd approach (Most optimized)
+
+Time: O(n), where n is the length of the given string s2
+Space: O(1), in-place, though we are using an array(charFreq) of size 26, but still the size will remain same for
+all cases as the array size of 26 is fixed and doesn't depend on the input
+
+*/
+
+class Solution {
+public:
+  bool checkInclusion(string s1, string s2) {
+
+    int s1Len = s1.length();
+    int s2Len = s2.length();
+
+    if(s1Len > s2Len) {
+      return false;
+    }
+
+    int charFreq[26] = {};
+    for(int i = 0; i < s1Len; ++i) {
+      ++charFreq[s1[i] - 'a'];
+    }
+
+    int i = 0;
+    int j = 0;
+    int totalChars = 0;
+    while(j < s2Len) {
+
+      if(charFreq[s2[j] - 'a']-- > 0) {
+        ++totalChars;
+      }
+
+      if(totalChars == s1Len) {
+        return true;
+      }
+
+      if(++j - i == s1Len){
+
+        if(charFreq[s2[i] - 'a']++ >= 0) {
+          --totalChars;
+        }
+
+        ++i;
+      }
+    }
+
+    return false;
   }
 };
--- Java/PermutationinString.java
+++ Java/PermutationinString.java
@@ -2,8 +2,10 @@
 
 Source: https://leetcode.com/problems/permutation-in-string/
 
-Time: O(n * m), where n and m are the lengths of the given string s2 and total number lower case english letters(26) respectively
-Space: O(n + k), we need char array for both the string i.e. s1 and s2
+1st approach
+
+Time: O(m * n), where m is total number lower case english letters(26) and n is the length of the given string s2
+Space: O(n + k), we need char array for both the string i.e. s2 and s1
 
 */
 
@@ -49,5 +51,108 @@ class Solution {
     }
 
     return true;
+  }
+}
+
+/*
+
+2nd approach (More optimized as we are using only a single array(charFreq))
+
+Time: (n * m), where m is total number lower case english letters(26) and n is the length of the given string s2
+Space: O(1), in-place, though we are using an array(charFreq) of size 26, but still the size will remain same for
+all cases as the array size of 26 is fixed and doesn't depend on the input
+
+*/
+
+class Solution {
+  public boolean checkInclusion(String s1, String s2) {
+
+    int s1Len = s1.length();
+    int s2Len = s2.length();
+
+    if(s1Len > s2Len) {
+      return false;
+    }
+
+    char[] charFreq = new char[26];
+    for(int i = 0; i < s1Len; ++i) {
+      ++charFreq[s1.charAt(i) - 'a'];
+      --charFreq[s2.charAt(i) - 'a'];
+    }
+
+    for(int i = s1Len; i < s2Len; ++i) {
+
+      if(isPerm(charFreq)) {
+        return true;
+      }
+
+      --charFreq[s2.charAt(i) - 'a'];
+      ++charFreq[s2.charAt(i - s1Len) - 'a'];
+    }
+
+    return isPerm(charFreq);
+  }
+
+  private boolean isPerm(char[] charFreq) {
+
+    for(int i = 0; i < 26; ++i) {
+
+      if(charFreq[i] != 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+/*
+
+3rd approach (Most Optimized)
+
+Time: O(n), where n is the length of the given string s2
+Space: O(1), in-place, though we are using an array(charFreq) of size 26, but still the size will remain same for
+all cases as the array size of 26 is fixed and doesn't depend on the input
+
+*/
+class Solution {
+  public boolean checkInclusion(String s1, String s2) {
+
+    int s1Len = s1.length();
+    int s2Len = s2.length();
+
+    if(s1Len > s2Len) {
+      return false;
+    }
+
+    int[] charFreq = new int[26];
+    for(int i = 0; i < s1Len; ++i) {
+      ++charFreq[s1.charAt(i) - 'a'];
+    }
+
+    int i = 0;
+    int j = 0;
+    int totalChars = 0;
+    while(j < s2Len) {
+
+      if(charFreq[s2.charAt(j) - 'a']-- > 0) {
+        ++totalChars;
+      }
+
+      if(totalChars == s1Len) {
+        return true;
+      }
+
+      if(++j - i == s1Len) {
+
+        if(charFreq[s2.charAt(i) - 'a']++ >= 0) {
+          --totalChars;
+        }
+
+        ++i;
+      }
+    }
+
+    return false;
   }
 }
