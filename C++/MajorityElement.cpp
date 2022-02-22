6a9e0ec2c2c6ba568895c26bc24a0f81a0e9a517
akshayaamar <akshayaamar05@gmail.com>
2022-02-22 16:49:30 +0530

    different approaches added for Majority Element program

--- C++/MajorityElement.cpp
+++ C++/MajorityElement.cpp
@@ -2,6 +2,37 @@
 
 Source: https://leetcode.com/problems/majority-element/
 
+Approach 1 (using extra space by taking map)
+
+Time: O(n), where n is the length of the given vector(nums)
+Space: O(n), map is required to store the frequency of each element
+
+*/
+
+class Solution {
+public:
+  int majorityElement(vector<int>& nums) {
+
+    unordered_map<int, int> map;
+    int majorityElement = 0;
+    int halfSize = nums.size() >> 1;
+
+    for(int num : nums) {
+
+      if(++map[num] > halfSize) {
+        majorityElement = num;
+        break;
+      }
+    }
+
+    return majorityElement;
+  }
+};
+
+/*
+
+Approach 2 (More optimized than approach 1 without using map)
+
 Time: O(n), where n is the length of the given vector(nums)
 Space: O(1), in-place
 
@@ -22,6 +53,39 @@ public:
       }
 
       count += (nums[i] == majorityElement) ? 1 : -1;
+    }
+
+    return majorityElement;
+  }
+};
+
+/*
+
+Slight modification to make it more optimized than approach 2
+
+Time: O(n), where n is the length of the given vector(nums)
+Space: O(1), in-place
+
+*/
+
+class Solution {
+public:
+  int majorityElement(vector<int>& nums) {
+
+    int majorityElement = nums[0];
+    int count = 1;
+    int size = nums.size();
+
+    for(int i = 1; i < size; ++i) {
+
+      if(count == 0) {
+        majorityElement = nums[i];
+        count = 1;
+      } else if(nums[i] != majorityElement) {
+        --count;
+      } else {
+        ++count;
+      }
     }
 
     return majorityElement;
--- Java/MajorityElement.java
+++ Java/MajorityElement.java
@@ -2,6 +2,38 @@
 
 Source: https://leetcode.com/problems/majority-element/
 
+Approach 1 (using extra space by taking map)
+
+Time: O(n), where n is the length of the given vector(nums)
+Space: O(n), map is required to store the frequency of each element
+
+*/
+
+class Solution {
+  public int majorityElement(int[] nums) {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int majorityElement = 0;
+    int halfLen = nums.length >> 1;
+
+    for(int num : nums) {
+
+      map.put(num, map.getOrDefault(num, 0) + 1);
+
+      if(map.get(num) > halfLen) {
+        majorityElement = num;
+        break;
+      }
+    }
+
+    return majorityElement;
+  }
+}
+
+/*
+
+Approach 2 (More optimized than approach 1 without using map)
+
 Time: O(n), where n is the length of the given vector(nums)
 Space: O(1), in-place
 
@@ -21,6 +53,38 @@ class Solution {
       }
 
       count += (nums[i] == majorityElement) ? 1 : -1;
+    }
+
+    return majorityElement;
+  }
+}
+
+/*
+
+Slight modification to make it more optimized than approach 2
+
+Time: O(n), where n is the length of the given vector(nums)
+Space: O(1), in-place
+
+*/
+
+class Solution {
+  public int majorityElement(int[] nums) {
+
+    int majorityElement = nums[0];
+    int count = 1;
+    int len = nums.length;
+
+    for(int i = 1; i < len; ++i) {
+
+      if(count == 0) {
+        majorityElement = nums[i];
+        count = 1;
+      }  else if(nums[i] != majorityElement) {
+        --count;
+      } else {
+        ++count;
+      }
     }
 
     return majorityElement;
