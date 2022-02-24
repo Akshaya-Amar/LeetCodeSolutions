d13121afdb159039e7fa531b379d63b383d46bae
akshayaamar <akshayaamar05@gmail.com>
2022-02-24 23:45:40 +0530

    ExcelSheetColumnNumber program added

---
+++ C++/ExcelSheetColumnNumber.cpp
@@ -0,0 +1,22 @@
+/*
+
+Source: https://leetcode.com/problems/excel-sheet-column-number/
+
+Time: O(n), where n is the length of the given string(columnTitle)
+Space: O(1), in-place
+
+*/
+
+class Solution {
+public:
+  int titleToNumber(string columnTitle) {
+
+    int columnNumber = 0;
+    for(char ch : columnTitle) {
+      int digit = ch - 'A' + 1;
+      columnNumber = (columnNumber * 26) + digit;
+    }
+
+    return columnNumber;
+  }
+};
---
+++ Java/ExcelSheetColumnNumber.java
@@ -0,0 +1,23 @@
+/*
+
+Source: https://leetcode.com/problems/excel-sheet-column-number/
+
+Time: O(n), where n is the length of the given string(columnTitle)
+Space: O(1), in-place
+
+*/
+
+class Solution {
+  public int titleToNumber(String columnTitle) {
+
+    int columnNumber = 0;
+    int len = columnTitle.length();
+
+    for(int i = 0; i < len; ++i) {
+      int digit = columnTitle.charAt(i) - 'A' + 1;
+      columnNumber = (columnNumber * 26) + digit;
+    }
+
+    return columnNumber;
+  }
+}
