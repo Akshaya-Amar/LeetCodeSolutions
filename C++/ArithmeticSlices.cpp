b774daae87272abd38dd8df1a905da29e1c6f71b
akshayaamar <akshayaamar05@gmail.com>
2022-03-05 20:02:19 +0530

    C++ program added for arithmeticslices problem

--- C++/ArithmeticSlices.cpp
+++ C++/ArithmeticSlices.cpp
@@ -12,9 +12,9 @@ Space: O(1), in-place
 */
 
 class Solution {
-  public int numberOfArithmeticSlices(int[] nums) {
-
-    int len = nums.length;
+public:
+  int numberOfArithmeticSlices(vector<int>& nums) {
+    int len = nums.size();
     int arithmeticSubArrays = 0;
 
     for(int i = 0; i < len - 2; ++i) {
@@ -32,7 +32,7 @@ class Solution {
 
     return arithmeticSubArrays;
   }
-}
+};
 
 --------------------------------------------------------------------------------------------------------------------------------
 
@@ -46,23 +46,24 @@ Space: O(1), in-place
 */
 
 class Solution {
-  public int numberOfArithmeticSlices(int[] nums) {
+public:
+  int numberOfArithmeticSlices(vector<int>& nums) {
 
-    int len = nums.length;
+    int len = nums.size();
     int arithmeticSubArrays = 0;
 
     for(int i = 0; i < len - 2; ++i) {
 
-      int diff = nums[i + 1] - nums[i];
+      int diff = nums[i] - nums[i + 1];
 
-      for(int j = i + 2; j < len && nums[j] - nums[j - 1] == diff; ++j) {
+      for(int j = i + 1; j < len - 1 && (nums[j] - nums[j + 1]) == diff; ++j) {
         ++arithmeticSubArrays;
       }
     }
 
     return arithmeticSubArrays;
   }
-}
+};
 
 --------------------------------------------------------------------------------------------------------------------------------
 
@@ -76,11 +77,13 @@ Space: O(n), an array of size equal to the length of given array(nums) is requir
 */
 
 class Solution {
-  public int numberOfArithmeticSlices(int[] nums) {
+public:
+  int numberOfArithmeticSlices(vector<int>& nums) {
 
-    int len = nums.length;
+    int len = nums.size();
     int arithmeticSubArrays = 0;
-    int[] dp = new int[len];
+    int dp[len];
+    memset(dp, 0, sizeof(dp));
 
     for(int i = 2; i < len; ++i) {
 
@@ -92,14 +95,15 @@ class Solution {
 
     return arithmeticSubArrays;
   }
-}
+};
 
 --------------------------------------------------------------------------------------------------------------------------------
 
 /*
 
-Approach 4 (more optimised than Approach 3 by eliminating extra array for space)
+Approach 4 (space optimised Approach by eliminating extra array for space)
 
+Basic idea
 1. Minimum 3 indices are required to make arithmetic progression
 2. Starting with index 2, if we found 2 same differences i.e. if the diff of 2 consecutive elements is same as diff of previous 2 consecutive elements,
 it means that we have got the arithmetic sequence and the running sequence gets extended upto ith index, so increment the curr by 1 and add this curr to arithmeticSubArrays
@@ -111,10 +115,11 @@ Space: O(1), in-place
 */
 
 class Solution {
-  public int numberOfArithmeticSlices(int[] nums) {
+public:
+  int numberOfArithmeticSlices(vector<int>& nums) {
 
+    int len = nums.size();
     int curr = 0;
-    int len = nums.length;
     int arithmeticSubArrays = 0;
 
     for(int i = 2; i < len; ++i) {
@@ -128,7 +133,7 @@ class Solution {
 
     return arithmeticSubArrays;
   }
-}
+};
 
 --------------------------------------------------------------------------------------------------------------------------------
 
@@ -142,9 +147,10 @@ Space: O(1), in-place
 */
 
 class Solution {
-  public int numberOfArithmeticSlices(int[] nums) {
+public:
+  int numberOfArithmeticSlices(vector<int>& nums) {
 
-    int len = nums.length;
+    int len = nums.size();
     int arithmeticSubArrays = 0;
 
     for(int i = 2; i < len; ++i) {
@@ -159,5 +165,6 @@ class Solution {
     }
 
     return arithmeticSubArrays;
+
   }
-}
+};
