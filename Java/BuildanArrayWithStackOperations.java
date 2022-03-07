4a76e763534cd6aa835c955a88d3cc94e6ad8656
akshayaamar <akshayaamar05@gmail.com>
2022-03-07 23:48:12 +0530

    Brief explanation added for BuildanArrayWithStackOperations program

--- Java/BuildanArrayWithStackOperations.java
+++ Java/BuildanArrayWithStackOperations.java
@@ -2,6 +2,10 @@
 
 Source: https://leetcode.com/problems/build-an-array-with-stack-operations/
 
+Basic idea:
+- Check the array elements with first N numbers till the length of the array
+- If number is exist in array, then simply do "push" operation, else do "push" and "pop" operations
+
 Time: O(n), where n is the provided n
 Space: O(n), we need a list to store the operations(i.e. "push", "pop")
 
@@ -12,11 +16,11 @@ class Solution {
 
     List<String> operationsNeeded = new ArrayList<>();
     int len = target.length;
-    int num = 0;
+    int input = 0;
 
     for(int i = 0; i < len; ++i) {
 
-      while(++num != target[i]) {
+      while(++input != target[i]) {
         operationsNeeded.add("Push");
         operationsNeeded.add("Pop");
       }
