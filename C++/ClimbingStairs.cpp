0fae167aeefcc52c75570ee6462ad22ac1a28bcc
akshayaamar <akshayaamar05@gmail.com>
2022-03-09 22:56:44 +0530

    Climbing Stairs program added

---
+++ C++/ClimbingStairs.cpp
@@ -0,0 +1,104 @@
+/*
+
+Source: https://leetcode.com/problems/climbing-stairs/
+
+Approach 1
+
+Time: O(n), where n is the given n
+Space: O(n), an array is needed to store the steps
+
+*/
+
+class Solution {
+public:
+  int climbStairs(int n) {
+
+    int ways[n + 1];
+    ways[0] = 1;
+    ways[1] = 1;
+
+    for(int i = 2; i <= n; ++i) {
+      ways[i] = ways[i - 1] + ways[i - 2];
+    }
+
+    return ways[n];
+  }
+};
+
+-------------------------------------------------------------------------------------------------------
+
+/*
+
+Approach 2 (without using extra array)
+
+Time: O(n), where n is the given n
+Space: O(1), in-place
+
+*/
+
+class Solution {
+public:
+  int climbStairs(int n) {
+
+    int first = 1;
+    int second = 1;
+    int ways = 0;
+
+    for(int i = 1; i < n; ++i) {
+      ways = first + second;
+      first = second;
+      second = ways;
+    }
+
+    return second;
+  }
+};
+
+-------------------------------------------------------------------------------------------------------
+
+/*
+
+Approach 3 (performing less operations (i.e. no swapping))
+
+Time: O(n), where n is the given n
+Space: O(1), in-place
+
+*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        int ways[] = {1, 1};
+
+        for(int i = 2; i <= n; ++i) {
+          ways[i % 2] = ways[0] + ways[1];
+        }
+
+        return ways[n % 2];
+     }
+};
+
+-------------------------------------------------------------------------------------------------------
+
+/*
+
+Approach 4 (same as Approach 3 but using bitwise operator)
+
+Time: O(n), where n is the given n
+Space: O(1), in-place
+
+*/
+class Solution {
+public:
+  int climbStairs(int n) {
+
+    int ways[] = {1, 1};
+
+    for(int i = 2; i <= n; ++i) {
+      ways[i & 1] = ways[0] + ways[1];
+    }
+
+    return ways[n & 1];
+  }
+};
