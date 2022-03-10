44ad956633e099ea23a12e3ec86037145319245e
akshayaamar <akshayaamar05@gmail.com>
2022-03-10 23:19:00 +0530

    Best Time to Buy and Sell Stock II

---
+++ Java/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,36 @@
+/*
+
+Source: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+Time: O(n), where n is the size of the given array(prices)
+Space: O(1), in-place
+
+*/
+
+class Solution {
+  public int maxProfit(int[] prices) {
+
+    int profit = 0;
+    int buy = 0;
+    int sell = 0;
+    int days = prices.length;
+
+    while(buy < days && sell < days) {
+      while(buy < days - 1 && prices[buy + 1] < prices[buy]) {
+        buy++;
+      }
+
+      sell = buy;
+
+      while(sell < days - 1 && prices[sell + 1] > prices[sell]) {
+        sell++;
+      }
+
+      profit += prices[sell] - prices[buy];
+
+      buy = sell + 1;
+    }
+
+    return profit;
+  }
+}
