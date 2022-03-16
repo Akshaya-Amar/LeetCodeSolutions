70885e76b27f0c0a1a321f22eb1762bc96b0dd6e
akshayaamar <akshayaamar05@gmail.com>
2022-03-16 23:05:56 +0530

    O(1) space program added for  Second Largest Digit in a String for java

--- Java/SecondLargestDigitinaString.java
+++ Java/SecondLargestDigitinaString.java
@@ -2,8 +2,10 @@
 
 Source: https://leetcode.com/problems/second-largest-digit-in-a-string/
 
+2 Approaches
+
 Time: O(n), where n is the size of the given string s
-Space: O(1), in-place
+Space: O(n), char array is needed to check for the character if it's a digit
 
 */
 
@@ -23,6 +25,47 @@ class Solution {
           secondMax = max;
           max = digit;
         } else if (digit > secondMax && digit < max) {
+          secondMax = digit;
+        }
+      }
+    }
+
+    return secondMax;
+  }
+
+  private boolean isDigit(char ch) {
+    return ch >= '0' && ch <= '9';
+  }
+}
+
+/*
+
+2nd approach
+
+Time: O(n), where n is the size of the given string s
+Space: O(1), in-place
+
+*/
+
+class Solution {
+  public int secondHighest(String s) {
+
+    int max = -1;
+    int secondMax = -1;
+    int len = s.length();
+
+    for(int i = 0; i < len; ++i) {
+
+      char ch = s.charAt(i);
+
+      if(ch >= '0' && ch <= '9') {
+
+        int digit = ch - '0';
+
+        if(digit > max) {
+          secondMax = max;
+          max = digit;
+        } else if(digit > secondMax && digit < max) {
           secondMax = digit;
         }
       }
