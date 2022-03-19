0cd92ddecd6c0e4dcee1d5a2ccbd666152137fe7
akshayaamar <akshayaamar05@gmail.com>
2022-03-19 19:15:55 +0530

    one more approach added for ReplaceWords

--- Java/ReplaceWords.java
+++ Java/ReplaceWords.java
@@ -2,6 +2,7 @@
 
 Source: https://leetcode.com/problems/replace-words/
 
+Approach 1 (Brute force)
 Time: O(m * n), where m is the length of the given string(sentence) and n is the length of list(dictionary)
 Space: O(n), StringBuilder is needed to store the words
 
@@ -15,6 +16,49 @@ class Solution {
     StringBuilder res = new StringBuilder();
 
     for(String word : split) {
+
+      for(String root : dictionary) {
+        if(word.startsWith(root)) {
+          word = root;
+        }
+      }
+
+      if(res.length() != 0) {
+        res.append(' ');
+      }
+
+      res.append(word);
+    }
+
+    return res.toString();
+  }
+}
+
+/*
+
+Approach 2 (Without using split())
+
+Time: O(m * n), where m is the length of the given string(sentence) and n is the length of list(dictionary)
+Space: O(n), StringBuilder is needed to store the words
+
+*/
+
+class Solution {
+  public static String replaceWords(List<String> dictionary, String sentence) {
+
+    StringBuilder res = new StringBuilder();
+    char[] sentenceChars = sentence.toCharArray();
+    int len = sentence.length();
+
+    for(int i = 0; i < len; ++i) {
+
+      int firstIndex = i;
+
+      while(i < len && sentenceChars[i] != ' ') {
+        ++i;
+      }
+
+      String word = String.valueOf(sentenceChars, firstIndex, i - firstIndex);
 
       for(String root : dictionary) {
         if(word.startsWith(root)) {
