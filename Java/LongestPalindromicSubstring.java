5b82f8330b0727d614fc064834d953b61a20fd78
akshayaamar <akshayaamar05@gmail.com>
2022-03-22 21:26:03 +0530

    More approaches added for longestpalindromicsubstring

--- Java/LongestPalindromicSubstring.java
+++ Java/LongestPalindromicSubstring.java
@@ -2,7 +2,58 @@
 
 Source: https://leetcode.com/problems/longest-palindromic-substring/
 
-Time: O(n ^ 2), where n is the length of the given String(s)
+Approach 1 (Brute Force Approach, but will give TLE)
+
+Time: O(n ^ 3), where n is the length of the given String(s)
+Space: O(1), in-place
+
+*/
+
+class Solution {
+
+  public String longestPalindrome(String s) {
+
+    int maxLen = 0;
+    int left = 0;
+    int len = s.length();
+
+    for(int i = 0; i < len; ++i) {
+
+      for(int j = i; j < len; ++j) {
+
+        String sub = s.substring(i, j + 1);
+        if(isPalindrome(sub)) {
+          int subLen = sub.length();
+          if(subLen > maxLen) {
+            left = i;
+            maxLen = subLen;
+          }
+        }
+      }
+    }
+
+    return s.substring(left, left + maxLen);
+  }
+
+  private boolean isPalindrome(String sub) {
+
+    int start = 0;
+    int end = sub.length() - 1;
+
+    while(start < end && sub.charAt(start) == sub.charAt(end)) {
+      ++start;
+      --end;
+    }
+
+    return start >= end;
+  }
+}
+
+/*
+
+Approach 2 (Brute force too, but acceptable)
+
+Time: O(n ^ 3), where n is the length of the given String(s)
 Space: O(1), in-place
 
 */
@@ -40,7 +91,7 @@ class Solution {
             maxLen = j - i;
             longestSub = s.substring(i, j + 1);
 
-            // no need to iterate unnecessary eg: "abba", longest can't be more than len 4(i.e. "abba" or "aabba")
+            // no need to iterate unnecessary eg: "abba", longest can't be more than length 4(i.e. "abba" or "aabba")
             if(longestSub.length() + i == len) {
               break outer;
             }
@@ -50,5 +101,48 @@ class Solution {
     }
 
     return longestSub;
+  }
+}
+
+/*
+
+Approach 3
+
+Time: O(n ^ 2), where n is the length of the given String(s)
+Space: O(1), in-place
+
+*/
+
+class Solution {
+
+  private int start;
+  private int maxLen;
+  private int len;
+
+  public String longestPalindrome(String s) {
+
+    len = s.length();
+
+    for(int i = 0; i < len; ++i) {
+      extendPalindrome(s, i, i);
+      extendPalindrome(s, i, i + 1);
+    }
+
+    return s.substring(++start, start + maxLen);
+  }
+
+  private void extendPalindrome(String s, int left, int right) {
+
+    while(left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
+      --left;
+      ++right;
+    }
+
+    int subLen = right - left - 1;
+
+    if(subLen > maxLen) {
+      maxLen = subLen;
+      start = left;
+    }
   }
 }
