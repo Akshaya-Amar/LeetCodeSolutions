3c88f47eb13a4d5e1cd1c940275f8827026c6a19
akshayaamar <akshayaamar05@gmail.com>
2022-03-29 23:16:43 +0530

    Partitioning Into Minimum Number Of Deci-Binary Numbers solution added in readme

--- Java/PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.java
+++ Java/PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.java
@@ -6,18 +6,46 @@ Basic Idea:
 Just return the max digit
 eg:
 n = 1234 (take as many one as the value of digits i.e. take 1 1's for digit 1, take 3 1's for digit 3, take 4 1's for digit 4 and so on)
-    1111
-  + 0111
-  + 0011
-  + 0001
-    ----
-    1234
-    ----
+1111
++ 0111
++ 0011
++ 0001
+----
+1234
+----
 
-1st Approach()
+1st Approach
 
 Time: O(n), where n is the length of the given string(n)
 Space: O(1), in-place
+
+*/
+
+class Solution {
+  public int minPartitions(String n) {
+
+    int max = 0;
+    int len = n.length();
+
+    for(int i = 0; i < len; ++i) {
+
+      int digit = n.charAt(i) - '0';
+
+      if(digit > max) {
+        max = digit;
+      }
+    }
+
+    return max;
+  }
+}
+
+/*
+
+2nd Approach (avoid charAt() repeatidly)
+
+Time: O(n), where n is the length of the given string(n)
+Space: O(n), char array is needed to access each character of the given string(s)
 
 */
 
@@ -44,7 +72,7 @@ class Solution {
 2nd Approach (faster than Approach 1, by avoiding char('8') to digit(8) conversion repeatidly i.e. avoiding int digit = ch - '0')
 
 Time: O(n), where n is the length of the given string(n)
-Space: O(1), in-place
+Space: O(n), char array is needed to access each character of the given string(s)
 
 */
 
