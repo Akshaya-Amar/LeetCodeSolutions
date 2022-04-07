13091e65f923f29886d0858d340c8e1aa770b2aa
akshayaamar <akshayaamar05@gmail.com>
2022-04-07 21:46:07 +0530

    one more solution added for MinimumNumberofOperationstoMoveAllBallstoEachBox

--- Java/MinimumNumberofOperationstoMoveAllBallstoEachBox.java
+++ Java/MinimumNumberofOperationstoMoveAllBallstoEachBox.java
@@ -59,15 +59,12 @@ class Solution {
       int count = 0;
 
       for(int j = i - 1; j >= 0; --j) {
-
         if(ch[j] == '1') {
           count += i - j;
         }
-
       }
 
       for(int j = i + 1; j < len; ++j) {
-
         if(ch[j] == '1') {
           count += j - i;
         }
@@ -82,6 +79,19 @@ class Solution {
 }
 
 /*
+
+Basic Idea:
+Move balls from left to right and track the number of operations took place by each ball for each box
+eg:
+String boxes = "1101"
+left to right operations - |0|1|3|5|
+0 - For 1st box(at index 0 of boxes), no operation took place
+1 - For 2nd box(at index 1 of boxes), 1 operation took place, i.e. to move ball in 1st box(index 0) to 2nd box(index 1)
+3 - For 3rd box(at index 2 of boxes), 3 operations took place, i.e. 2 operations to move ball in 1st box(index 0) to 3rd box(index 2) + 1 operation to move ball in 2nd box(index 1) to 3rd box(index 2)
+5 - For 4th box(at index 3 of boxes), 5 operations took place, i.e. 3 operations to move ball in 1st box(index 0) to 4th box(index 3) + 2 operations to move ball in 2nd box(index 1) to 4th box(index 3)
+
+Do, the same operation from right to left
+|5|2|1|0|
 
 Time: O(n), where n is the length of the given String(boxes)
 Space: O(n), char array is needed to access the each character of string
@@ -249,5 +259,38 @@ class Solution {
     }
 
     return left;
+  }
+}
+
+/*
+
+Time: O(n), where n is the length of the given String(boxes)
+Space: O(n), char array is needed to access the each character of string
+
+*/
+
+class Solution {
+  public int[] minOperations(String boxes) {
+
+    char[] ch = boxes.toCharArray();
+    int len = ch.length;
+    int[] answer = new int[len];
+    int count = ch[0] - '0';
+
+    for(int i = 1; i < len; ++i) {
+      answer[i] = answer[i - 1] + count;
+      count += ch[i] - '0';
+    }
+
+    count = ch[len - 1] - '0';
+    int operations = 0;
+
+    for(int i = len - 2; i >= 0; --i) {
+      operations += count;
+      count += ch[i] - '0';
+      answer[i] += operations;
+    }
+
+    return answer;
   }
 }
