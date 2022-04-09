77695b4ecc4f3f9c1a7e89f4cf645a4567aa666c
akshayaamar <akshayaamar05@gmail.com>
2022-04-09 22:02:39 +0530

    Group the People Given the Group Size They Belong To java solution added

---
+++ Java/GroupthePeopleGiventheGroupSizeTheyBelongTo.java
@@ -0,0 +1,37 @@
+/*
+
+Source: https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
+
+Time: O(n), where n is the length of the given array(groupSizes)
+Space: O(n), map is needed to store the group
+
+Basic Idea:
+Keep adding people in their corresponding group.
+If the group is filled completely, add to the list of groups and reset that group.
+
+*/
+
+class Solution {
+  public List<List<Integer>> groupThePeople(int[] groupSizes) {
+
+    List<List<Integer>> groups = new ArrayList<>();
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
+    int len = groupSizes.length;
+
+    for(int i = 0; i < len; ++i) {
+
+      List<Integer> group = map.getOrDefault(groupSizes[i], new ArrayList<>());
+      group.add(i); // keep adding people in their corresponding group
+
+      // If the group is filled completely,
+      if(group.size() == groupSizes[i]) {
+        groups.add(group); // add to the list of groups and
+        group = new ArrayList<>(); // reset that group
+      }
+
+      map.put(groupSizes[i], group);
+    }
+
+    return groups;
+  }
+}
