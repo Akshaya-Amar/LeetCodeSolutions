c285ac65eca5b5797a1498ae6c0584a9f4392819
akshayaamar <akshayaamar05@gmail.com>
2022-04-13 18:16:59 +0530

    Brief explanation added for Subdomain Visit Count java solution

--- Java/SubdomainVisitCount.java
+++ Java/SubdomainVisitCount.java
@@ -2,8 +2,17 @@
 
 Source: https://leetcode.com/problems/subdomain-visit-count/
 
+Basic Idea:
+
+- Create a map to store the count/frequency of each domain/subdomain
+- Pick a count paired domain from the the given array
+- Take out the visits and domain name and store them seperately
+- Store the domain name and visits in the map
+- Iterate through the full domain name and pick it's subdomains and store them too along with their visits
+- Iterate through the map and store all the count paired domains of each subdomain in the list one by one
+
 Time: O(n * m), where n is the length of the given String array(cpdomains) and m is the length of each String in the given array
-Space: O(n), Map is needed to store the frequency of paired domains
+Space: O(n), a map is needed to store the frequency of paired domains
 
 */
 
@@ -11,28 +20,32 @@ Space: O(n), Map is needed to store the frequency of paired domains
 class Solution {
   public List<String> subdomainVisits(String[] cpdomains) {
 
-    Map<String, Integer> count = new HashMap();
+    Map<String, Integer> subdomainsFreq = new HashMap<>();
 
-    for (String cd : cpdomains) {
-      int i = cd.indexOf(' ');
-      int n = Integer.valueOf(cd.substring(0, i));
-      String s = cd.substring(i + 1);
-      int sLen = s.length();
-      for (i = 0; i < sLen; ++i) {
-        if (s.charAt(i) == '.') {
-          String d = s.substring(i + 1);
-          count.put(d, count.getOrDefault(d, 0) + n);
+    for(String cpdomain : cpdomains) {
+
+      int index = cpdomain.indexOf(' ');
+      int visits = Integer.parseInt(cpdomain.substring(0, index));
+      String domain = cpdomain.substring(index + 1);
+      subdomainsFreq.put(domain, subdomainsFreq.getOrDefault(domain, 0) + visits);
+      int domainLen = domain.length();
+
+      for(int i = 0; i < domainLen; ++i) {
+        if(domain.charAt(i) == '.') {
+          String subdomain = domain.substring(i + 1);
+          subdomainsFreq.put(subdomain, subdomainsFreq.getOrDefault(subdomain, 0) + visits);
         }
       }
-      count.put(s, count.getOrDefault(s, 0) + n);
     }
 
-    List<String> res = new ArrayList();
-    StringBuilder subDomain = new StringBuilder();
-    for (String d : count.keySet()){
-      res.add(subDomain.append(count.get(d)).append(' ').append(d).toString());
-      subDomain.setLength(0);
+    List<String> res = new ArrayList<>();
+    StringBuilder countPairedSubdomain = new StringBuilder();
+    for(String subdomain : subdomainsFreq.keySet()) {
+      countPairedSubdomain.append(subdomainsFreq.get(subdomain)).append(' ').append(subdomain);
+      res.add(countPairedSubdomain.toString());
+      countPairedSubdomain.setLength(0);
     }
+
     return res;
   }
 }
